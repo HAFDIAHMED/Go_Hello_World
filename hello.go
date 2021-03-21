b41e8f8fdd25a222c28517cdfb0438bd20a4eafe
HAFDIAHMED <ahafdi62@gmail.com>
2021-03-21 16:03:53 +0100

    array

--- hello.go
+++ hello.go
@@ -2,27 +2,12 @@ package main
 
 import (
     "fmt"
-   "time"
+
 )
 func main() {
 	
-	i:=2
-    fmt.Print("write ",i," as : ")
-
-    switch i {
-    case 1: 
-    	fmt.Print("one\n")
-    case 2: 
-    	fmt.Print("two\n")
-    case 3: 
-    	fmt.Print("three\n")
-    }
-     switch time.Now().Weekday() {
-     case time.Saturday, time.Sunday:
-     	fmt.Println("it is weekend")
-     default :
-     	fmt.Println("it is a weekday")
-     }
+	var a[5] int 
+	fmt.Println("emp :",a)
 
     
 }
