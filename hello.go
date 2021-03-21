ee63ee420ff4cff0d5d7d202c4f6b2e30ac28edb
HAFDIAHMED <ahafdi62@gmail.com>
2021-03-21 12:58:23 +0100

    new changes

--- hello.go
+++ hello.go
@@ -4,15 +4,8 @@ import "fmt"
 
 func main() {
 	
-	var x float32 =20.0
-	y:=20
-	fmt.Println(x)
-	fmt.Printf("x is the type of  %T \n",x  )
-	fmt.Println(y)
-	fmt.Printf("y is the type of %T \n", y)
-    //fmt.Println("Hello, World!")
-    //fmt.Println("im wrinting my first code with hello world !!!");
-    //hello_world()
+	
+    fmt.Printf("hello \\ world ")
     
 }
 func hello_world(){
