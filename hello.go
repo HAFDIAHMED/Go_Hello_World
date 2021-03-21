1bc2f3f2a7e3f594f97ffaa0e924a970d26d425b
HAFDIAHMED <ahafdi62@gmail.com>
2021-03-21 12:38:54 +0100

    first func

--- hello.go
+++ hello.go
@@ -4,17 +4,18 @@ import "fmt"
 
 func main() {
 	/*this is my firt hello world code */
-	var int somme1;
-	var int somme2;
+	var int somme1=1;
+	var int somme2=2;
 
-    fmt.Println("Hello, World!")
-    fmt.Println("im wrinting my first code with hello world !!!");
+    //fmt.Println("Hello, World!")
+    //fmt.Println("im wrinting my first code with hello world !!!");
     //hello_world()
+    somme(somme1,somme2)
 }
 func hello_world(){
 
 	fmt.Println("hello 2")
 }
 func   somme(int n1, int n2){
-		return n1+n1
+		fmt.Println(n1+n2)
 }
