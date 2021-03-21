433d5e8d48e6d9564198c5716f2bc939c0e22ab1
HAFDIAHMED <ahafdi62@gmail.com>
2021-03-21 12:41:22 +0100

    float 32

--- hello.go
+++ hello.go
@@ -3,19 +3,16 @@ package main
 import "fmt"
 
 func main() {
-	/*this is my firt hello world code */
-	var int somme1=1;
-	var int somme2=2;
+	
+	var x float32
+
 
     //fmt.Println("Hello, World!")
     //fmt.Println("im wrinting my first code with hello world !!!");
     //hello_world()
-    somme(somme1,somme2)
+    
 }
 func hello_world(){
 
 	fmt.Println("hello 2")
-}
-func   somme(int n1, int n2){
-		fmt.Println(n1+n2)
 }
